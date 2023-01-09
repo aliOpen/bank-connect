e6eb287f082e4c778291c8770240aceed9322a34
aliOpen <109346245+aliOpen@users.noreply.github.com>
2023-01-09 17:56:17 +0530

    added todo list in note detail component

--- apps/notepad/src/app/component/note-details/note-details.component.ts
+++ apps/notepad/src/app/component/note-details/note-details.component.ts
@@ -1,8 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { arrayBuffer } from 'stream/consumers';
+import { Note } from '../notes-list/note.model';
+import { Task } from './task.model';
 
 @Component({
   selector: 'np-note-details',
   templateUrl: './note-details.component.html',
   styleUrls: ['./note-details.component.scss'],
 })
-export class NoteDetailsComponent {}
+export class NoteDetailsComponent implements OnInit {
+  notesList: Note[] = [];
+  currentNoteDetail: any;
+  todos: Task[] = [];
+  taskName: string = '';
+  taskCompleted: boolean = false;
+
+  constructor(private route: ActivatedRoute) {}
+  ngOnInit() {
+    const routerUniqueId = this.route.snapshot.params['id'];
+    const initialList = localStorage.getItem('storelist');
+    if (initialList) {
+      this.notesList = JSON.parse(initialList);
+    }
+    for (let i = 0; i < this.notesList.length; i++) {
+      if (routerUniqueId === this.notesList[i]._id) {
+        this.currentNoteDetail = this.notesList[i];
+        console.log(this.currentNoteDetail);
+      }
+    }
+  }
+
+  onAddTodo() {
+    if (!this.taskName) return;
+    if (!this.currentNoteDetail.todoList) this.currentNoteDetail.todoList = [];
+    this.currentNoteDetail.todoList.push({
+      name: this.taskName,
+      completed: this.taskCompleted,
+    });
+    localStorage.setItem('storelist', JSON.stringify(this.notesList));
+  }
+}
--- apps/notepad/src/app/component/notes-list/note.model.ts
+++ apps/notepad/src/app/component/notes-list/note.model.ts
@@ -1,9 +1,11 @@
+import { Task } from '../note-details/task.model';
+
 export interface Note {
   _id: string;
   title: string;
   content: string;
   color?: string;
-  todoList?: Array<string>;
+  todoList?: Array<Task>;
   createdAt: Date;
   updatedAt?: Date;
   attachment?: Blob;
