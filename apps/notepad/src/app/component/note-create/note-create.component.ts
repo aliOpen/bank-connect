68a3fe86b14da31f0a0ac948b3ebda537a5caf9b
aliOpen <109346245+aliOpen@users.noreply.github.com>
2023-01-16 18:02:27 +0530

    note-list-color

--- apps/notepad/src/app/component/note-create/note-create.component.ts
+++ apps/notepad/src/app/component/note-create/note-create.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { NotesService } from '../../notes.service';
@@ -10,12 +10,15 @@ import { Note } from '../notes-list/note.model';
   styleUrls: ['./note-create.component.scss'],
 })
 export class NoteCreateComponent {
+  @ViewChild('colorElement') colorElement!: ElementRef;
   createNoteForm: FormGroup = new FormGroup({
     title: new FormControl(null),
     content: new FormControl(null),
+    color: new FormControl('bg-white'),
   });
   notesList: Note[] = [];
   showFullForm: boolean = false;
+  showColorPalette: boolean = false;
 
   constructor(private notesService: NotesService, private router: Router) {}
 
@@ -26,13 +29,25 @@ export class NoteCreateComponent {
       title: this.createNoteForm.value.title,
       content: this.createNoteForm.value.content,
       createdAt: new Date(),
+      color: this.createNoteForm.value.color,
     });
 
     localStorage.setItem('storelist', JSON.stringify(this.notesList));
 
     this.router.navigate(['']);
   }
+  onColor() {
+    // this.createNoteForm.patchValue({ color: 'bg-blue-100' });
+    this.showColorPalette = !this.showColorPalette;
+  }
   showForm() {
     this.showFullForm = true;
+  }
+
+  getColor() {
+    return this.createNoteForm.get('color')?.value;
+  }
+  onColorList(color1: string) {
+    this.createNoteForm.patchValue({ color: color1 });
   }
 }
