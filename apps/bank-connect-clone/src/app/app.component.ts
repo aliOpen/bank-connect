97ce5a656fb2924e3c70da1cbff0d18dbd94858c
aliOpen <109346245+aliOpen@users.noreply.github.com>
2023-02-15 18:00:25 +0530

    reactive form for a login page

--- apps/bank-connect-clone/src/app/app.component.ts
+++ apps/bank-connect-clone/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+import { FormGroup, FormControl } from '@angular/forms';
+
 @Component({
   selector: 'ali-assignments-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'bank-connect-clone';
+  loginPageForm: FormGroup = new FormGroup({
+    username: new FormControl(),
+    password: new FormControl(),
+  });
+
+  onSubmit() {
+    console.log('Form Submitted');
+  }
 }
--- apps/bank-connect-clone/src/app/app.module.ts
+++ apps/bank-connect-clone/src/app/app.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
   ],
   providers: [],
