d93f22293f6cb7413dcf8867b7afdf20b7a74f4a
aliOpen <109346245+aliOpen@users.noreply.github.com>
2023-03-10 19:22:13 +0530

    Login component, dashboard component

--- apps/bank-connect-clone/src/app/login/login.component.ts
+++ apps/bank-connect-clone/src/app/login/login.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
-import { FormGroupDirective, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'ali-assignments-login',
--- apps/bank-connect-clone/tailwind.config.js
+++ apps/bank-connect-clone/tailwind.config.js
@@ -1,6 +1,17 @@
 const { createGlobPatternsForDependencies } = require('@nrwl/angular/tailwind');
 const { join } = require('path');
 
+// const colors = {
+//   secondary: '#F99D27',
+//   yellow: {
+//     500: '#FFD148',
+//   },
+//   grey: {
+//     100: '#FBC47D',
+//     50: '#F9F9F9',
+//     25: '#FFFFFF',
+//   },
+// };
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
